2ee260865a0f1c04f89b96480249f1cd25f4936b
Mustafa Said Kocatepe <msaidkocatepe@gmail.com>
2021-12-14 15:40:12 +0300

    --comm

--- service/user/user.go
+++ service/user/user.go
@@ -6,26 +6,26 @@ import (
 	"github.com/mustafakocatepe/go-api-example/model"
 )
 
-func GetUserByUserName(slice []model.User, val string) (*model.User, bool) {
-	for _, item := range slice {
-		if item.UserName == val && item.IsActive {
+func GetUserByUserName(users []model.User, id string) (*model.User, bool) {
+	for _, item := range users {
+		if item.UserName == id && item.IsActive {
 			return &item, true
 		}
 	}
 	return nil, false
 }
-func GetUsers(slice []model.User) ([]model.User, bool) {
+func GetUsers(users []model.User) ([]model.User, bool) {
 	var responseModel []model.User
-	for _, item := range slice {
+	for _, item := range users {
 		if item.IsActive {
 			responseModel = append(responseModel, item)
 		}
 	}
 	return responseModel, true
 }
-func DeleteUserByUserId(slice []model.User, val string) bool {
-	for i, item := range slice {
-		value, _ := strconv.Atoi(val)
+func DeleteUserByUserId(users []model.User, id string) bool {
+	for i, item := range users {
+		value, _ := strconv.Atoi(id)
 		if item.UserId == value && item.IsActive {
 			(model.UserArray[i]).IsActive = false
 			return true
@@ -33,9 +33,9 @@ func DeleteUserByUserId(slice []model.User, val string) bool {
 	}
 	return false
 }
-func UpdateUserNameByUserId(slice []model.User, val string, userName string) bool {
-	for i, item := range slice {
-		value, _ := strconv.Atoi(val)
+func UpdateUserNameByUserId(users []model.User, id string, userName string) bool {
+	for i, item := range users {
+		value, _ := strconv.Atoi(id)
 		if item.UserId == value && item.IsActive {
 			(model.UserArray[i]).UserName = userName
 			return true
@@ -44,9 +44,9 @@ func UpdateUserNameByUserId(slice []model.User, val string, userName string) boo
 	return false
 }
 
-func UpdateUserByUserId(slice []model.User, val string, user model.User) bool {
-	for i, item := range slice {
-		value, _ := strconv.Atoi(val)
+func UpdateUserByUserId(users []model.User, id string, user model.User) bool {
+	for i, item := range users {
+		value, _ := strconv.Atoi(id)
 		if item.UserId == value && item.IsActive {
 
 			if len(user.UserName) != 0 {
@@ -56,7 +56,6 @@ func UpdateUserByUserId(slice []model.User, val string, user model.User) bool {
 			if len(user.Surname) != 0 {
 				(model.UserArray[i]).Surname = user.Surname
 			}
-
 			return true
 		}
 	}
